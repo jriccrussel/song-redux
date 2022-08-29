575e761f2c0fc9c63a2f6d6581a6392fb8fd4b81
jriccrussel <jriccrussel@gmail.com>
2022-08-29 17:17:57 +0800

    notes how we have access sa state and dispatch

--- src/components/songlist.js
+++ src/components/songlist.js
@@ -2,19 +2,21 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 export class SongList extends Component {
-  render() {
-    return (
-      <div>SongList</div>
-    )
-  }
+    render() {
+        // naa natai access sa state and sa dispatch, we have access state because sa mapStateToProps
+        console.log(this.props)
+        return (
+        <div>SongList</div>
+        )
+    }
 }
 
 // hold sa state or data from reducer 
 // how do we have access sa state its because ddto sa index.js nag hatag ta ug "Provider" and nag add pud ta ug props ma "store={createStore(reducers)}"
-const mapStateToProps = (state) => {
-    console.log(state)
+const mapStateToProps = state => {
+    // console.log(state)
 
-    return state
+    return {song: state.songs}
 }
 
 export default connect(mapStateToProps)(SongList)

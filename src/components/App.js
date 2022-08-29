3cf96974e29e6d4a7c384c9f9d915034758983f6
jriccrussel <jriccrussel@gmail.com>
2022-08-29 16:55:40 +0800

    add connect function from redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from 'react'
+import SongList from './songlist'
 
 const App = () => {
-  return <div>App</div>;
-};
+  return (
+    <div>
+      <SongList />
+    </div>
+  )
+}
 
-export default App;
+export default App

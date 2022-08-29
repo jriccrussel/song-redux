bea7af9ade5e43c13a3d31ec4b037d6e9b5cd0ec
jriccrussel <jriccrussel@gmail.com>
2022-08-29 20:09:58 +0800

    add styling to songDetail

--- src/components/songdetails.js
+++ src/components/songdetails.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const SongDetails = (props) => {
-    console.log(props)
+// const SongDetails = (props) => {
+//     // console.log(props)
+//     return (
+//         <div>SongDetails</div>
+//     )
+// }
+
+const SongDetails = ({ song }) => {
+    if(!song){
+        return <div>Select a song</div>
+    }
+    
     return (
-        <div>SongDetails</div>
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br />
+                Duration: {song.duration}
+            </p>
+        </div>
     )
 }
 
+// para maka access ta sa new/updated na state(inig click sa button na kuha nato ang new/updated na state then g kuha nato para maka access ta)
 const mapStateToProps = state => {
-    console.log(state)
+    // console.log(state)
     return { song: state.selectedSong }
 }
 
